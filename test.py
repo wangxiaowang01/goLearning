67091e281d2ce31eec442a7df4cfb37b35766ea2
wangxiaowang01 <tiamwang@163.com>
2024-07-19 18:42:55 +0800

    test 002

---
+++ test.py
@@ -0,0 +1,6 @@
+
+lista = [1,2,3,4,5]
+for i in lista:
+    a = 1
+    a*=i
+    print(a)
---
+++ unit1/main.go
@@ -0,0 +1,20 @@
+// Echo2 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+)
+
+var number int64 = 1
+var name string = "Alice"
+var height float64 = 181.56
+var number2 = 1
+var number3 int64
+
+func main() {
+	// s, sep := "t", "a"
+	// lista := []string{"1", "2", "4", "5"}
+
+	fmt.Println(number, name, height, number2, number3)
+	// fmt.Println(s)
+}
