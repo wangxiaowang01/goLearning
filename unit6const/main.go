de88e5d0b9d3636af627b3e6b6a3a8f36e0d1cd7
wangxiaowang01 <tiamwang@163.com>
2024-07-24 18:57:33 +0800

    update note

--- unit6const/main.go
+++ unit6const/main.go
@@ -14,6 +14,22 @@ const (
 	Saturday
 )
 
+type Flags uint
+
+const (
+	FlagUp           Flags = 1 << iota // is up
+	FlagBroadcast                      // supports broadcast access capability
+	FlagLoopback                       // is a loopback interface
+	FlagPointToPoint                   // belongs to a point-to-point link
+	FlagMulticast                      // supports multicast access capability
+)
+
 func main() {
-	fmt.Printf("Friday %v\n", Friday)
+	// fmt.Printf("Friday %v\n", Friday)
+	fmt.Printf("Flags %v\n", FlagUp)
+	fmt.Printf("FlagBroadcast %v\n", FlagBroadcast)
+	fmt.Printf("FlagLoopback %v\n", FlagLoopback)
+	fmt.Printf("FlagPointToPoint %v\n", FlagPointToPoint)
+	fmt.Printf("FlagMulticast %v\n", FlagMulticast)
+
 }
