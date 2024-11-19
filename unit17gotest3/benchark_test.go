e92c779f6bfa52cd83ea7b42f3f51c0f2fa6d830
wangxiaowang01 <tiamwang@163.com>
2024-11-19 22:57:36 +0800

    :rocket:​ add unit17 go test benchark test

---
+++ unit17gotest3/benchark_test.go
@@ -0,0 +1,18 @@
+package unit17gotest
+
+import "testing"
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+
+	}
+}
+func IsPalindrome(s string) bool {
+	for i := range s {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
