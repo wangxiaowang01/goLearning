2d147f27bc5ce75f6881de2462918fbf06d20f82
wangxiaowang01 <tiamwang@163.com>
2024-11-19 22:52:44 +0800

    update unit16 select case

---
+++ unit16select/main.go
@@ -0,0 +1,32 @@
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch1 <- "from ch1"
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch2 <- "from ch2"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		}
+	}
+}
+
